924aa799b76ddd448d3d7b3c653bae19c488e711
DigesSuvagiya <suvagiyadigesh1284@gmail.com>
2026-03-17 11:21:38 +0530

    env port

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const emailRoutes = require("./routes/email");
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" })); // limit for base64 pdf
+app.use(express.json({ limit: "10mb" }));
 
 app.use("/api/email", emailRoutes);
 app.use("/api/sheet", sheetRoutes);
 
 app.get("/", (req, res) => res.send("Utility Backend Running"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
