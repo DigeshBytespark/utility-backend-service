a454add18353f054c02a3fba87dd7589189cc301
DigesSuvagiya <suvagiyadigesh1284@gmail.com>
2026-03-17 16:21:56 +0530

    arham consultancy linked

--- config/projects.js
+++ config/projects.js
@@ -7,5 +7,14 @@ module.exports = {
       pass: process.env.SMTP_PASS,
     },
     sheetUrl: process.env.SHEET_URL_BLINKCARBON  // ← add this
+  },
+  arham: {
+    smtp: {
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+    sheetUrl: process.env.SHEET_URL_ARHAM
   }
 };
